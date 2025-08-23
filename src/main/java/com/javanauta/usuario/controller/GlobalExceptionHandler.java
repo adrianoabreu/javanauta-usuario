0edd8d090d7c6c67a503feac18d7b2bb1bcceb9e
Adriano Abreu <adriano.luis.machado2013@hotmail.com>
2025-08-23 10:32:25 -0300

    Ajuste na classe GlobalExceptionHandle ao importar o IllegalArgumentException correto

--- src/main/java/com/javanauta/usuario/controller/GlobalExceptionHandler.java
+++ src/main/java/com/javanauta/usuario/controller/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.javanauta.usuario.controller;
 
 import com.javanauta.usuario.infrastructure.exceptions.ConflictException;
+import com.javanauta.usuario.infrastructure.exceptions.IllegalArgumentException;
 import com.javanauta.usuario.infrastructure.exceptions.ResourceNotFoundException;
 import com.javanauta.usuario.infrastructure.exceptions.UnauthorizedException;
 import org.springframework.http.HttpStatus;
